0f0cf12ea3a579dec8006c7d350f9d9d42d5bc14
sadewole <samueladewole15@gmail.com>
2022-10-22 15:34:59 +0100

    fix(component): added truncate to long text in song card

--- components/SongCard.component.tsx
+++ components/SongCard.component.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import _ from 'lodash';
 import { Image, Text, View } from 'react-native';
 import { MusicData } from './types';
+
+const truncateText = (text: string) => {
+  return _.truncate(text, {
+    length: 35,
+  });
+};
 
 export function SongCard({
   shrink,
@@ -16,8 +23,12 @@ export function SongCard({
         resizeMode="cover"
         className={`w-full ${shrink ? 'h-40' : 'h-48'} mb-2 rounded-md`}
       />
-      <Text className="text-white font-poppins">{data.title}</Text>
-      <Text className="text-gray-500 font-poppins">{data.subtitle}</Text>
+      <Text className="text-white font-poppins">
+        {truncateText(data.title)}
+      </Text>
+      <Text className="text-gray-500 font-poppins truncate">
+        {truncateText(data.subtitle)}
+      </Text>
     </View>
   );
 }
